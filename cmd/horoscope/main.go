942f06e7ac7e6aed552186e53b64835ae0fa6eac
Luc Perkins <lucperkins@gmail.com>
2022-10-03 11:24:16 -0500

    First commit; Nix setup plus Go service

---
+++ cmd/horoscope/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    routes := gin.Default()
+    routes.NoRoute(func(c *gin.Context) {
+        c.String(http.StatusOK, "Hello world")
+    })
+    routes.Run()
+}
---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Deploying Nix to Nomad";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ nomad ];
+      };
+    });
+}
