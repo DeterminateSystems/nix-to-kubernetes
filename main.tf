d6adc7b3722dd38447f80cbb9a0a2b34e57b20ed
Luc Perkins <lucperkins@gmail.com>
2022-10-03 13:26:54 -0500

    Add DO SSH key

--- main.tf
+++ main.tf
@@ -4,7 +4,12 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "2.22.1"
     }
   }
+}
+
+resource "digitalocean_ssh_key" "nomad" {
+  name       = "nomad"
+  public_key = file("~/.ssh/nix-nomad.pub")
 }
