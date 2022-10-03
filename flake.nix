4d07f41e19740ef8649f0dd18b7fd76d72722b1e
Luc Perkins <lucperkins@gmail.com>
2022-10-03 11:44:54 -0500

    Begin setting up Actions pipeline

--- flake.nix
+++ flake.nix
@@ -1,15 +1,66 @@
 {
   description = "Deploying Nix to Nomad";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }:
     let
-      pkgs = import nixpkgs {
-        inherit system;
+      name = "horoscope";
+      goVersion = 19;
+      goOverlay = self: super: {
+        go = super."go_1_${toString goVersion}";
       };
+      overlays = [ goOverlay ];
     in
-    {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ nomad ];
-      };
-    });
+
+    # Specific to CI environment
+    flake-utils.lib.eachSystem [ "x86_64-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit overlays;
+            system = "x86_64-linux";
+          };
+        in
+        {
+          packages = rec {
+            default = horoscope;
+
+            horoscope = pkgs.buildGoModule {
+              inherit name;
+              src = ./.;
+              vendorSha256 = pkgs.lib.fakeHash;
+              CGO_ENABLED = 0;
+            };
+
+            docker =
+              let
+                run = "${horoscope}/bin/linux_amd64/${name}";
+              in
+              pkgs.dockerTools.buildLayeredImage {
+                inherit name;
+
+                config.Cmd = [ run ];
+              };
+          };
+        })
+
+    //
+
+    # Cross-platform logic
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ goOverlay ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                go
+                nomad
+              ];
+          };
+        });
 }
