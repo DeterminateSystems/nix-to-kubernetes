576f30d83fb40fe505f5ac9f55f294a0c005f04a
Luc Perkins <lucperkins@gmail.com>
2022-10-06 12:17:36 -0500

    Remove Terraform setup

--- flake.nix
+++ flake.nix
@@ -68,7 +68,6 @@
 
                 # DevOps
                 nomad
-                terraform
               ];
           };
         });
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "nomad-client-ip" {
-  value = digitalocean_droplet.nomad-client.ipv4_address
-}
