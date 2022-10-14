bd84b06731fcebeb9097cdc31caa66ede3cfb009
Luc Perkins <lucperkins@gmail.com>
2022-10-13 21:57:46 -0500

    More flake.nix inline docs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Deploying Nix to Nomad";
+  description = "Deploying Nix-build Docker images to Kubernetes";
 
   inputs = {
     nixpkgs.url = "nixpkgs";
@@ -8,6 +8,7 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
+      # Application metadata
       name = "horoscope";
 
       image = {
@@ -16,34 +17,44 @@
         owner = "DeterminateSystems";
       };
 
+      # The hash for the service's Go dependencies
       vendorSha256 = "sha256-fwJTg/HqDAI12mF1u/BlnG52yaAlaIMzsILDDZuETrI=";
 
+      # An overlay for exposing a specific Go version as pkgs.go
       goVersion = 19;
-      goOverlay = self: super: {
+      goOverlay = self: super: rec {
         go = super."go_1_${toString goVersion}";
       };
+
+      # An overlay for specifying the Terraform version
       terraformOverlay = self: super: {
         terraform = super.terraform.overrideAttrs (_: {
           version = "1.3.2";
         });
       };
+
+      # The combined overlays
       overlays = [ goOverlay terraformOverlay ];
     in
 
-    # Per-system outputs
+    # Per-system outputs for the default systems here:
+      # https://github.com/numtide/flake-utils/blob/master/default.nix#L3-L9
     flake-utils.lib.eachDefaultSystem
       (system:
         let
+          # Custom Nixpkgs for the current system and with overlays applied
           pkgs = import nixpkgs {
             inherit overlays system;
           };
 
+          # Import the CI scripts into the dev environment
           ci = import ./nix/ci.nix { inherit pkgs; };
         in
         {
           packages = rec {
             default = horoscope;
 
+            # Our Go service as a standard Go module
             horoscope = pkgs.buildGoModule {
               inherit name vendorSha256;
               src = ./.;
@@ -63,7 +74,9 @@
               };
           };
 
+          # Cross-platform development environment (including CI)
           devShells.default = pkgs.mkShell {
+            # Packages available in the environment
             buildInputs = ci ++ (with pkgs;
               [
                 # Golang
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module github.com/DeterminateSystems/nix-to-nomad
+module github.com/DeterminateSystems/nix-to-kubernetes
 
-go 1.18
+go 1.19
+
+require github.com/gin-gonic/gin v1.8.1
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
