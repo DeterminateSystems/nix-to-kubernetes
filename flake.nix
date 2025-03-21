a354a73aa25c1ebf7d88b02568dd0f4eb54e09b0
Luc Perkins <lucperkins@gmail.com>
2025-03-21 12:52:06 -0300

    Modernize repo

--- flake.nix
+++ flake.nix
@@ -4,19 +4,26 @@
   description = "Deploying Nix-built Docker images to Kubernetes";
 
   # Flake inputs (the Nix code sources we need to rely on here)
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
 
   # Flake outputs (the stuff we want to provide in this flake)
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs:
     let
-      # Application metadata
-      name = "horoscope";
+      # Helpers for per-system outputs
+      forSystem = s: f: inputs.nixpkgs.lib.genAttrs s (system: f {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          # Allow unfree packages like Terraform
+          config.allowUnfree = true;
+        };
+      });
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forEachSupportedSystem = forSystem supportedSystems;
 
       image = {
-        inherit name;
+        name = "horoscope";
         registry = "ghcr.io";
         owner = "DeterminateSystems";
       };
@@ -26,64 +33,53 @@
       # change, we need to update this.
       vendorHash = "sha256-cNcwAG7DliZbgb3AE7y8c8I2C/O1iaXHTdaHkJGtoEs=";
     in
+    {
+      # Cross-platform development environment (including CI)
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          # Packages available in the environment
+          packages = with pkgs;
+            [
+              # Golang
+              go_1_22
+              gotools # goimports, etc.
 
-    # Per-system outputs for the default systems here:
-      # https://github.com/numtide/flake-utils/blob/master/default.nix#L3-L9
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          # Custom Nixpkgs for the current system
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
+              # Utilities
+              jq
+              graphviz # For visualizing the Terraform graph
+              httpie # For arbitrary HTTP calls
+
+              # DevOps
+              doctl # DigitalOcean CLI
+              kubectl # Kubernetes CLI
+              kubectx # Kubernetes context management utility
+              terraform # Infrastructure as code FTW
+            ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }: {
+        # Our Go service as a standard Go module (Go 1.19)
+        horoscope = pkgs.buildGoModule {
+          name = "horoscope";
+          inherit vendorHash;
+          src = ./.;
+        };
+      });
+
+      # Intended only for CI. The image fails to run if you build it on a
+      # non-`x86_64-linux` system, despite the build succeeding. There are
+      # ways around this in Nix but in this case we only need to build the
+      # image in CI.
+      dockerImages = forEachSupportedSystem ({ pkgs }: {
+        horoscope = pkgs.dockerTools.buildLayeredImage {
+          name = "${image.registry}/${image.owner}/${image.name}";
+          config = {
+            Cmd = [ "${inputs.self.packages.x86_64-linux.horoscope}/bin/horoscope" ];
+            ExposedPorts."8080/tcp" = { };
           };
-        in
-        {
-          packages = rec {
-            # Enables us to build the Go service by running plain old `nix build`
-            default = horoscope;
-
-            # Our Go service as a standard Go module (Go 1.19)
-            horoscope = pkgs.buildGo122Module {
-              inherit name vendorHash;
-              src = ./.;
-            };
-
-            # Intended only for CI. The image fails to run if you build it on a
-            # non-`x86_64-linux` system, despite the build succeeding. There are
-            # ways around this in Nix but in this case we only need to build the
-            # image in CI.
-            docker =
-              pkgs.dockerTools.buildLayeredImage {
-                name = "${image.registry}/${image.owner}/${image.name}";
-                config = {
-                  Cmd = [ "${horoscope}/bin/${name}" ];
-                  ExposedPorts."8080/tcp" = { };
-                };
-                maxLayers = 120;
-              };
-          };
-
-          # Cross-platform development environment (including CI)
-          devShells.default = pkgs.mkShell {
-            # Packages available in the environment
-            packages = with pkgs;
-              [
-                # Golang
-                go_1_22
-                gotools # goimports, etc.
-
-                # Utilities
-                jq
-                graphviz # For visualizing the Terraform graph
-                httpie # For arbitrary HTTP calls
-
-                # DevOps
-                doctl # DigitalOcean CLI
-                kubectl # Kubernetes CLI
-                kubectx # Kubernetes context management utility
-                terraform # Infrastructure as code FTW
-              ];
-          };
-        });
+          maxLayers = 120;
+        };
+      });
+    };
 }
